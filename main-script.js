aeb049432de79be15a961da2ba1fdd55c59f1a9c
BaconBao <baconbao@users.noreply.github.com>
2021-10-02 16:21:08 +0800

    feat: add main script

---
+++ main-script.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Gmail - Remove emoji on mail list
+// @name:zh-TW   Gmail - 移除信件列表中的表情符號
+// @name:zh-CN   Gmail - 移除邮件列表中的表情符号
+// @version      1.0.0
+// @description  Remove emoji on the Gmail's mail list
+// @author       BaconBao
+// @namespace    https://github.com/baconbao/
+// @match        https://mail.google.com/mail/*
+// @icon         https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico
+// @grant        GM_addStyle
+// @compatible  chrome
+// @compatible  firefox
+// @compatible  Tampermonkey
+// ==/UserScript==
+
+GM_addStyle(`
+    tr[data-inboxsdk-thread-row] img[data-emoji] {
+        display: none;
+    }
+`);

e78902fd9d50dea5bc0d957e2065dd3f55f8b11d
BaconBao <baconbao@users.noreply.github.com>
2021-10-02 16:35:20 +0800

    fix: update script info and upgrade

--- main-script.js
+++ main-script.js
@@ -1,17 +1,19 @@
 // ==UserScript==
-// @name         Gmail - Remove emoji on mail list
-// @name:zh-TW   Gmail - 移除信件列表中的表情符號
-// @name:zh-CN   Gmail - 移除邮件列表中的表情符号
-// @version      1.0.0
-// @description  Remove mail titles' emoji on Gmail.
-// @author       BaconBao
-// @namespace    https://github.com/baconbao/
-// @match        https://mail.google.com/mail/*
-// @icon         https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico
-// @grant        GM_addStyle
-// @compatible  chrome
-// @compatible  firefox
-// @compatible  Tampermonkey
+// @name               Gmail - Remove emoji on mail list
+// @name:zh-TW         Gmail - 移除信件列表中的表情符號
+// @name:zh-CN         Gmail - 移除邮件列表中的表情符号
+// @version            1.0.1
+// @description        Remove mail titles' emoji on Gmail.
+// @description:zh-TW  在Gmail信件列表中移除表情符號
+// @description:zh-CN  在Gmail邮件列表中移除表情符号
+// @author             BaconBao
+// @namespace          https://github.com/baconbao/
+// @match              https://mail.google.com/mail/*
+// @icon               https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico
+// @grant              GM_addStyle
+// @compatible         chrome
+// @compatible         firefox
+// @compatible         Tampermonkey
 // ==/UserScript==
 
 GM_addStyle(`
